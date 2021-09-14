bdcace0685e2cbe1713f422619513c2fd0554f40
TechoMonkey <90660877+TechoMonkey@users.noreply.github.com>
2021-09-13 20:00:45 -0500

    Add files via upload

---
+++ C++ Homework 1.cpp
@@ -0,0 +1,58 @@
+// C++ Homework 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<iostream>
+#include<string>
+#include <regex>
+
+using namespace std;
+
+void ProblemOne(int a, int b)
+{
+	int count = a;
+	while (count <= 1000)
+	{
+		while (count % b == 0)
+		{
+			cout << count << '\t';
+			break;
+		}
+		count++;
+	}
+}
+void ProblemTwo()
+{
+    //Input string
+    string str = "Hello World";
+
+    //Initialize reverse with null string value
+    string rev = "";
+
+    //Iterate input string in revrese order
+    for (int i = str.length() - 1; i >= 0; i--) {
+        //concatenate characters to the reverse
+        rev = rev + str[i];
+    }
+	cout << str<< endl;
+    cout << rev;
+ 
+}
+void ProblemThree(string str)
+{
+
+   
+}
+
+
+int main()
+{
+	cout << "Problem One"<< endl;
+	ProblemOne(5,10);
+	cout << endl <<"Problem Two" << endl;
+	ProblemTwo();
+	cout << endl<< "Problem Three" << endl;
+	ProblemThree("the cake is a lie");
+	
+	
+	return 0;
+}
+
